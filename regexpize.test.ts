53cae17d6b5dd07386bbdfb1968bba3c5d3cffa1
askirmas <kirmas@phystech.edu>
2020-03-20 16:13:41 +0200

    Add regexpize function for easier handling regexps

---
+++ regexpize.test.ts
@@ -0,0 +1,22 @@
+import {regexpize} from './regexpize'
+const str = 'asd';
+[
+  undefined,
+  '',
+  {global: true},
+  {g: true}
+].forEach(flag => myIt(flag))
+
+function myIt(flags: Parameters<typeof regexpize>[1]) {
+  return it(
+    flags === undefined
+    ? 'undefined'
+    : typeof flags === 'string'
+    ? flags
+    : JSON.stringify(flags), () => {
+      const result = regexpize(str, flags)
+      expect(regexpize(str, flags)).toStrictEqual(result) 
+      expect(regexpize(result)).toStrictEqual(result)     
+  })
+
+}
---
+++ regexpize.ts
@@ -0,0 +1,47 @@
+export {
+  regexpize
+}
+
+const regExpFlaging = {
+  "global": "g",
+  "ignoreCase": "i",
+  "multiline": "m",
+  "dotAll": "s",
+  "unicode": "u",
+  "sticky": "y"
+} 
+
+/** https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions */
+export type RegExpFlags = Partial<{
+  /** Global search */
+  "global": boolean
+  /** Case-insensitive search */
+  "ignoreCase": boolean
+  /** Multi-line search */
+  "multiline": boolean
+  /** Allows `.` to match newline characters. (Added in ES2018, not yet supported in Firefox) */
+  "dotAll": boolean
+  /** "unicode"; treat a pattern as a sequence of unicode code points */
+  "unicode": boolean
+  /** Perform a "sticky" search that matches starting at the current position in the target string. See [sticky](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/sticky) */
+  "sticky": boolean    
+}>
+
+function regexpize(regexp: string|RegExp, flags?: string|RegExpFlags) {
+  return regexp instanceof RegExp
+  ? regexp
+  : new RegExp(regexp, flagsToString(flags))
+}
+
+function flagsToString(flags?: string|RegExpFlags) {
+  if (typeof flags === 'string')
+    return flags
+  if (!flags || typeof flags !== 'object')
+    return undefined
+  let $return = ''
+  , key: keyof typeof flags
+  for (key in flags)
+    if (key in regExpFlaging && flags[key])
+      $return += regExpFlaging[key]
+  return $return
+}
