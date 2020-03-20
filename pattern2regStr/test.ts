b2cb2e32d6a8862f97f343e5834e27a2b38bdc2e
askirmas <kirmas@phystech.edu>
2020-03-20 19:46:50 +0200

    Reuse $default.value in tests of patter2regStr

--- pattern2regStr/test.ts
+++ pattern2regStr/test.ts
@@ -4,7 +4,7 @@ import $default from './default.json'
 const params = {prefix: "${", postfix: "}"}
 , keyReg = keyReger(params)
 , pattern = '${x}/b/${y}'
-, freeRegString = '(?<x>.*)/b/(?<y>.*)'
+, freeRegString = `(?<x>${$default.value})/b/(?<y>${$default.value})`
 , regString = `^${freeRegString}$`
 it('pre and post', () => expect(pattern2regStr(
   pattern,
