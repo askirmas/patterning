01589d18f0943a54aaded7f485bce0f4ccc67a85
askirmas <kirmas@phystech.edu>
2020-03-21 17:02:09 +0200

    Segregate research stuff

--- fn.test.ts
+++ fn.test.ts
@@ -1,7 +1,5 @@
 
 import {keyReger, schemaParser, parse, methods, SchemaParameters} from './fn'
-import regexpize from './regexpize'
-import { schema2regStr, schema2replace } from './schemaReg'
 import {expressRoute, templateLiteral, definitions} from "./test.json"
 
 const {withValueFree, instance} = definitions
@@ -83,88 +81,3 @@ it('bad method', () => expect(parse(
 )).toBe(
   undefined
 ))
-
-
-describe('research', () => {
-
-  type rp = Parameters<typeof regexpize>
-  function exec(str: string, schema: rp[0], flags?: rp[1]) {
-    const  $return = regexpize(schema, flags)
-    .exec(str)
-    return $return && {...$return.groups}
-  }
-
-  describe('tag as backreference', () => {
-    // https://2ality.com/2017/05/regexp-named-capture-groups.html#backreferences
-    const key = "tag"
-    , keyPattern = '[a-z]+'
-    , valuePattern = "[^<]+"
-    , openPrefix = "<"
-    , openPostfix = ">"
-    , closePrefix = "</"
-    , closePostfix = ">"
-    , catcher = `${
-      openPrefix}(?<${key}>${keyPattern})${openPostfix
-    }${
-      valuePattern
-    }${
-      closePrefix}\\k<${key}>${closePostfix
-    }`
-    it('true', () => expect(exec("<title>abc</title>", catcher)).toStrictEqual({
-      "tag": "title"
-    }))
-    it('false', () => expect(exec("<title>abc</titl>", catcher)).toBe(null))
-    it('html array', () => expect(
-      [
-        ..."X<title>abc</title>Y<b>b</b>Z"
-        .matchAll(
-          /<(?<tag>[a-z]+)>(?<text>[^<]*)<\/\k<tag>>/g
-        )
-      ].map(({groups}) => ({...groups}))
-    ).toStrictEqual([
-      {tag: "title", text: "abc"},
-      {tag: "b", text: "b"}
-    ]))
-    it('html tags', () => expect(
-      [
-        ..."X<title>abc</title>Y<b>b</b>Z"
-        .matchAll(/[^<]*<(?<tag>[a-z]+)>.*<\/\1>[^<]*/g)
-      ].map(({groups}) => ({...groups}))
-    ).toStrictEqual([{tag: "title"}, {tag: "b"}]))
-    it('html trick', () => expect(
-      () => "X<title>abc</title>Y<b>b</b>Z"
-        .matchAll(/[^<]*<([a-z]+)>(?<\1>.*)<\/\1[^<]*/g)
-    ).toThrowError(SyntaxError))
-    it('html', () => expect(
-      [
-        ..."X<title>abc</title>Y<b>b</b>Z"
-        .matchAll(/^([^<]*<(?<tag>[a-z]+)>(.*)<\/\k<tag>>[^<]*)*$/g)
-      ].map(({groups, ...etc}) => ({...etc, groups: {...groups}}))
-      [0].groups
-    ).toStrictEqual({tag: "b"}))
-  })
-  describe('reshape', () => {
-    const parser = regexpize(schema2regStr(
-      expressRouteCatcher,
-      expressRoute.schemas[1]
-    ))
-    describe('not match', () => {
-      it('replace', () => expect(
-        instance.replace(parser,
-          '$<source>/b/$<id>'
-        )
-      ).toBe('/b/'))
-    })    
-    it('idfn', () => expect(
-      instance.replace(parser,
-        schema2replace(expressRouteCatcher, expressRoute.schemas[1])
-      )
-    ).toBe(instance))
-    it('1', () => expect(
-      instance.replace(parser,
-        '/$<x>?id=$<y>'
-      )
-    )
-    .toBe('/a?id=c'))
-  })
-})
---
+++ research.test.ts
@@ -0,0 +1,60 @@
+import regexpize from "./regexpize"
+
+describe('research', () => {
+  type rp = Parameters<typeof regexpize>
+  function exec(str: string, schema: rp[0], flags?: rp[1]) {
+    const  $return = regexpize(schema, flags)
+    .exec(str)
+    return $return && {...$return.groups}
+  }
+
+  describe('tag as backreference', () => {
+    // https://2ality.com/2017/05/regexp-named-capture-groups.html#backreferences
+    const key = "tag"
+    , keyPattern = '[a-z]+'
+    , valuePattern = "[^<]+"
+    , openPrefix = "<"
+    , openPostfix = ">"
+    , closePrefix = "</"
+    , closePostfix = ">"
+    , catcher = `${
+      openPrefix}(?<${key}>${keyPattern})${openPostfix
+    }${
+      valuePattern
+    }${
+      closePrefix}\\k<${key}>${closePostfix
+    }`
+    it('true', () => expect(exec("<title>abc</title>", catcher)).toStrictEqual({
+      "tag": "title"
+    }))
+    it('false', () => expect(exec("<title>abc</titl>", catcher)).toBe(null))
+    it('html array', () => expect(
+      [
+        ..."X<title>abc</title>Y<b>b</b>Z"
+        .matchAll(
+          /<(?<tag>[a-z]+)>(?<text>[^<]*)<\/\k<tag>>/g
+        )
+      ].map(({groups}) => ({...groups}))
+    ).toStrictEqual([
+      {tag: "title", text: "abc"},
+      {tag: "b", text: "b"}
+    ]))
+    it('html tags', () => expect(
+      [
+        ..."X<title>abc</title>Y<b>b</b>Z"
+        .matchAll(/[^<]*<(?<tag>[a-z]+)>.*<\/\1>[^<]*/g)
+      ].map(({groups}) => ({...groups}))
+    ).toStrictEqual([{tag: "title"}, {tag: "b"}]))
+    it('html trick', () => expect(
+      () => "X<title>abc</title>Y<b>b</b>Z"
+        .matchAll(/[^<]*<([a-z]+)>(?<\1>.*)<\/\1[^<]*/g)
+    ).toThrowError(SyntaxError))
+    it('html', () => expect(
+      [
+        ..."X<title>abc</title>Y<b>b</b>Z"
+        .matchAll(/^([^<]*<(?<tag>[a-z]+)>(.*)<\/\k<tag>>[^<]*)*$/g)
+      ].map(({groups, ...etc}) => ({...etc, groups: {...groups}}))
+      [0].groups
+    ).toStrictEqual({tag: "b"}))
+  })
+})
